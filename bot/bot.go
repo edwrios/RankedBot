df19563dfb7134cafbb6026c9662095f6a7e8579
edwrios <rios.edwing@gmail.com>
2017-05-17 01:36:51 -0400

    Actualizado comando !ready. Cambia apodo de un miembro para indicar estado de "listo".

--- bot/bot.go
+++ bot/bot.go
@@ -15,6 +15,7 @@ import (
 
 var BotID string
 var goBot *discordgo.Session
+var GuildID = "313889492375175169"
 
 func Start() {
 
@@ -43,19 +44,28 @@ func Start() {
 func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 	var salida bytes.Buffer
 	var ok bool
+	var err error
 
 	if strings.HasPrefix(m.Content, config.BotPrefix) {
 		if m.Author.ID == BotID {
 			return
 		}
 		//var expresion = regexp.MustCompile(`!info\s.*`)
-		ok, _ = regexp.MatchString(`!info\s.*`, m.Content)
+		//ok, _ = regexp.MatchString(`!ready\s.*`, m.Content)
+		ok, _ = regexp.MatchString(`!ready`, m.Content)
 		if ok == true {
+			//var g *discordgo.Guild
+
+			var gm, _ = s.GuildMember(GuildID, m.Author.ID)
+
 			salida.WriteString("Author: ")
 			salida.WriteString(m.Author.String())
 			salida.WriteString("\n")
 			salida.WriteString("UserName: ")
 			salida.WriteString(m.Author.Username)
+			salida.WriteString("\n")
+			salida.WriteString("Nick: ")
+			salida.WriteString(gm.Nick)
 			salida.WriteString("\n")
 			salida.WriteString("ID: ")
 			salida.WriteString(m.Author.ID)
@@ -63,8 +73,26 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			salida.WriteString("Mensaje: ")
 			salida.WriteString(m.Content)
 			salida.WriteString("\n")
+
+			if gm.Nick == "" {
+				err = s.GuildMemberNickname(GuildID, m.Author.ID, m.Author.Username+" (R)")
+				if err != nil {
+					fmt.Println(err.Error())
+					_, _ = s.ChannelMessageSend(m.ChannelID, err.Error())
+					return
+				}
+			} else {
+				err = s.GuildMemberNickname(GuildID, m.Author.ID, gm.Nick+" (R)")
+				if err != nil {
+					fmt.Println(err.Error())
+					_, _ = s.ChannelMessageSend(m.ChannelID, err.Error())
+					return
+				}
+			}
+
 			_, _ = s.ChannelMessageSend(m.ChannelID, salida.String())
-			//_, _ = s.ChannelMessageSend("314110449824301056", "probando probando 123")
+			//_, _ = s.ChannelMessageSend("314110449824301056", "probando probando 123") //mensaje en canal #prueba
+
 		}
 	}
 	//fmt.Println(m.Content)
